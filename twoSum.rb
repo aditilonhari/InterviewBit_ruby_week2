8b30a0ef7e786c3d178f45a879e6ab2fe47a5445
Aditi Lonhari <lonhari.aditi@gmail.com>
2018-01-23 14:48:50 -0800

    Update twoSum.rb

--- twoSum.rb
+++ twoSum.rb
@@ -6,27 +6,7 @@ class Solution
         if a == nil || b == nil
             return nil
         end
-        
-        # a_map = {}
-        # a.each do |element|
-        #     a_map[element] = b - element
-        # end
-        
-        # start = []
-        # stop = []
-        # a_map.each do |key, value|
-        #     if a_map.has_key?(value)
-        #         start += [a.find_index(key)]
-        #         stop += [a.find_index(value)]
-        #     end
-        # end
-        
-        # return [] if start.empty?
-        # p start
-        # p stop
-        # return [start[0]+1, stop[0]+1]
-        
-        # this will find all combinations of 2 elements that add up to 9 
+    
         results = (0...a.size).to_a.combination(2).select { |first, last| 
             a[first] + a[last] == b
         }
